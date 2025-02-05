5d459be2e8659e469514724afbe7a162bc57534f
VioletDoodlez <nicolecovaliu@gmail.com>
2025-02-04 22:34:06 -0500

    feat: paddleRight moves in the opposite direction of the directional keys

--- Projects/pawngTemplate/Assets/Scripts/PaddleScript.cs
+++ Projects/pawngTemplate/Assets/Scripts/PaddleScript.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleScript : MonoBehaviour {
-    private float     yPos;
+    private float     xPos, yPos;
     public float      paddleSpeed = .05f;
 
     public KeyCode upKey, downKey;
-    public float topWall, bottomWall;
+    public float topWall, bottomWall, leftWall, rightWall;
 	
     // Start is called before the first frame update
     void Start() {
@@ -16,12 +16,44 @@ public class PaddleScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (gameObject.name == "paddleLeft") {
         if (Input.GetKey(downKey) && yPos > bottomWall) {
-                yPos -= paddleSpeed;
+            yPos -= paddleSpeed;
         }
 
         if (Input.GetKey(upKey)&& yPos < topWall) {
-                yPos += paddleSpeed;
+            yPos += paddleSpeed;
+        }
+        }
+
+        if (gameObject.name == "paddleRight") {
+        if (Input.GetKey(upKey) && yPos > bottomWall) {
+            yPos -= paddleSpeed;
+        }
+
+        if (Input.GetKey(downKey)&& yPos < topWall) {
+            yPos += paddleSpeed;
+        }
+        }
+
+        if (gameObject.name == "paddleTop") {
+        if (Input.GetKey(downKey) && xPos > leftWall) {
+            xPos -= paddleSpeed;
+        }
+
+        if (Input.GetKey(upKey)&& xPos < rightWall) {
+            xPos += paddleSpeed;
+        }
+        }
+
+        if (gameObject.name == "paddleBottom") {
+        if (Input.GetKey(downKey) && xPos > leftWall) {
+            xPos -= paddleSpeed;
+        }
+
+        if (Input.GetKey(upKey)&& xPos < rightWall) {
+            xPos += paddleSpeed;
+        }
         }
 
         transform.localPosition = new Vector3(transform.position.x, yPos, 0);
